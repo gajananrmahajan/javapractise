a19d28d2f037de75342b928f28ba28ee02ad93dc
Gajanan Mahajan <gajanan.forums@gmail.com>
2014-11-04 16:58:19 +0530

    Remove unnecessary imports

--- src/org/javatutorials/collections/UsefulCollectionMethods.java
+++ src/org/javatutorials/collections/UsefulCollectionMethods.java
@@ -2,9 +2,7 @@ package org.javatutorials.collections;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class UsefulCollectionMethods {
 
